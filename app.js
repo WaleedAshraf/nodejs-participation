9b934fc2d06c1c9f18148773a45726b211b5dc65
Waleed Ashraf <waleedashraf@outlook.com>
2018-04-14 22:55:12 +0500

    dir name log

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let app = express();
 
 let staticPath = path.join(__dirname, '/dist');
 app.use(express.static(staticPath));
-
+console.log('__dirname:',__dirname);
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/nodejs/TSC.html'));
 });
